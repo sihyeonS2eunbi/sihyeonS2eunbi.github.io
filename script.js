b507d1116eea98d3571f6eab08b87e334553b006
sihyeonS2eunbi <rnjstksaor@gmail.com>
2024-03-01 10:23:36 +0900

    init

---
+++ script.js
@@ -0,0 +1,44 @@
+/* scroll마다 한 페이지씩 이동하기 */
+window.onload = function () {
+  const elm = document.querySelectorAll('section');
+  const elmCount = elm.length;
+  elm.forEach(function (item, index) {
+    item.addEventListener('mousewheel', function (event) {
+      event.preventDefault();
+      let delta = 0;
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+        if (window.opera) delta = -delta;
+      } else if (event.detail) delta = -event.detail / 3;
+
+      let moveTop = window.scrollY;
+      let elmSelector = elm[index];
+
+      // wheel down : move to next section
+      if (delta < 0) {
+        if (elmSelector !== elmCount - 1) {
+          try {
+            moveTop =
+              window.pageYOffset +
+              elmSelector.nextElementSibling.getBoundingClientRect().top;
+          } catch (e) {}
+        }
+      }
+
+      // wheel up : move to previous section
+      else {
+        if (elmSelector !== 0 && elmSelector.id !== 'home') {
+          try {
+            moveTop =
+              window.pageYOffset +
+              elmSelector.previousElementSibling.getBoundingClientRect().top;
+          } catch (e) {}
+        }
+      }
+
+      const body = document.querySelector('html');
+      window.scrollTo({ top: moveTop, left: 0, behavior: 'smooth' });
+    });
+  });
+};
